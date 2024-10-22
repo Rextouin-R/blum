d892ab9f89eb42352d4b433e486dbd4909d78ab1
Rextouin-R <rattatouille1994@gmail.com>
2024-10-22 07:54:08 +0700

    Update user.js

--- src/services/user.js
+++ src/services/user.js
@@ -23,7 +23,7 @@ class UserService {
       .filter((line) => line.length > 0);
 
     if (users.length <= 0) {
-      console.log(colors.red(`Không tìm thấy dữ liệu user`));
+      console.log(colors.red(`Data pengguna tidak ditemukan`));
       return [];
     } else {
       let database = {};
